bb1fe02557aedf8d09c561030db394bae8b9baac
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-03-22 10:08:31 +0100

    22/03 10h07 Initialisation Class Modele

--- src/main/java/campus/Modele.java
+++ src/main/java/campus/Modele.java
@@ -1,4 +1,30 @@
 package campus;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Modele du Design Pattern MVC
+ * @author Stephane CHEVRIER
+ * @version 0.0
+ * @Date 2023-03-22
+ *
+ **/
 public class Modele {
+
+    /**
+     * Variables d'instance
+     */
+    private int NombreATrouver;
+
+
+    /**
+     * Constructeurs
+     * @param entierMini Entier mini
+     * @param entierMaxi Entier maxi
+     */
+    public Modele(int entierMini, int entierMaxi) {
+        Random entierAleatoire = new SecureRandom();
+        this.NombreATrouver = entierAleatoire.nextInt(entierMaxi-entierMini+1)+entierMini;
+    }
 }
